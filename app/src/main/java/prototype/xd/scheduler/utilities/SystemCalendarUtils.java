a8c78fa8d5a55ac3bf8ccc136ce15923ff6b1148
kloud-linux-xps13 <dgudim@gmail.com>
2022-03-10 17:06:29 +0300

    table print helper

--- app/src/main/java/prototype/xd/scheduler/utilities/SystemCalendarUtils.java
+++ app/src/main/java/prototype/xd/scheduler/utilities/SystemCalendarUtils.java
@@ -1,6 +1,7 @@
 package prototype.xd.scheduler.utilities;
 
 import static android.util.Log.INFO;
+import static java.lang.Math.max;
 import static prototype.xd.scheduler.utilities.Logger.log;
 import static prototype.xd.scheduler.utilities.PreferencesStore.preferences;
 import static prototype.xd.scheduler.utilities.QueryUtilities.query;
@@ -41,6 +42,46 @@ public class SystemCalendarUtils {
             CalendarContract.Events.EXDATE,
             CalendarContract.Events.DELETED,
             CalendarContract.Events.EVENT_TIMEZONE));
+    
+    public static void printTable(Cursor cursor) {
+        cursor.moveToFirst();
+        
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        ArrayList<Integer> column_sizes = new ArrayList<>();
+        String[] column_names = cursor.getColumnNames();
+        table.add(new ArrayList<>(Arrays.asList(column_names)));
+    
+        for (String column_name : column_names) {
+            column_sizes.add(column_name.length());
+        }
+        
+        for (int row = 0; row < cursor.getCount(); row++) {
+            ArrayList<String> record = new ArrayList<>();
+            for (int column = 0; column < column_names.length; column++) {
+                String column_val = cursor.getString(column);
+                record.add(column_val);
+                column_sizes.set(column, max(column_sizes.get(column), column_val.length()));
+            }
+            table.add(record);
+            cursor.moveToNext();
+        }
+    
+        for (int row = 0; row < table.size(); row++) {
+            for (int column = 0; column < column_names.length; column++) {
+                System.out.println(addSpaces(table.get(row).get(column), column_sizes.get(column) + 1));
+            }
+        }
+        
+        cursor.moveToFirst();
+    }
+    
+    public static String addSpaces(String input, int len){
+        StringBuilder out = new StringBuilder(input);
+        for(int i = input.length(); i < len; i++){
+            out.append(" ");
+        }
+        return out.toString();
+    }
     
     public static ArrayList<SystemCalendar> getAllCalendars(Context context, boolean loadMinimal) {
         ContentResolver contentResolver = context.getContentResolver();
