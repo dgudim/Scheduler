51441067ce982ab39e367eb91234830c77d6a01a
Kloud <34401005+dgudim@users.noreply.github.com>
2022-12-01 11:54:53 +0200

    Created utility script for fastlane validator

---
+++ scripts/check_trigger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+COMMIT_MSG=$(git log --no-merges -1 --oneline)
+
+# The commit marker "[fastlane]" or "[metadata]" will trigger the build when required
+if [[ "$GITHUB_EVENT_NAME" == schedule ||
+      "$GITHUB_EVENT_NAME" == workflow_dispatch ||
+      "$COMMIT_MSG" =~ \[fastlane\] ||
+      "$COMMIT_MSG" =~ \[metadata\] ]]; then
+    echo "::set-output name=build::true"
+fi
