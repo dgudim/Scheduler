3896017b979bc157ad0b2f93a16f91dda579f17a
kloud-sauron <dgudim@gmail.com>
2022-12-03 10:56:03 +0200

    Fix typo, spaces, version naming

--- fastlane/metadata/android/en-US/full_description.pl
+++ fastlane/metadata/android/en-US/full_description.pl
@@ -13,21 +13,23 @@ my $text = <DATA>;
 $text =~ s/[\x00-\x20]+/ /g;
 $text =~ s/\A //;
 $text =~ s/ \z//;
-$text =~ s/> />/g;
-$text =~ s/ </</g;
+
+$text =~ s/\/(p|div)> /\/$1>/g;
+$text =~ s/br> /br>/g;
+$text =~ s/ <(p|br|div)/<$1/g;
 
 open(my $fh,">:utf8",$file) or die "$file $!";
 say $fh $text;
 close($fh) or die "$file $!";
 
-# apt-cyg install tidy  libtidy5
+# apt-cyg install tidy libtidy5
 system qq(tidy -q -e $file);
 
 __DATA__
 <p><i>Scheduler</i> is a small app that shows your todo list on your lockscreen.</p>
 
 <p>
-Do you <b>always forget things</b> to do or <b>have a lot of them</b> to remeber?<br>
+Do you <b>always forget things</b> to do or <b>have a lot of them</b> to remember?<br>
 Want to <b>easily keep track</b> of them?<br>
 Then this app is for you, take <b>full advantage of your lockscreen real estate</b> and view your todo list right on it!<br>
 <b>No need to unlock the phone</b>, it's all there!
